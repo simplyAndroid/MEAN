d5b03d77d2a50a47973f166646c085d4e59419a7
ArunBharath Krishnan <abharath@ecentricarts.com>
2018-01-30 19:45:07 -0500

    Used ExpressJS-Ls2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -11,4 +11,13 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+});*/
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/home', (req, res) => res.send('welcome to home12'))
+
+app.listen(4000, () => console.log('Example app listening on port 4000!'))
